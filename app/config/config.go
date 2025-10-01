3687acbfe7f63a9a8f97e4d25e66a4c948e60e5a
developersajadur <itzmesojib@gmail.com>
2025-10-01 18:51:00 +0600

    add connection by psql

--- app/config/config.go
+++ app/config/config.go
@@ -9,6 +9,8 @@ import (
 
 type config struct {
 	Port      string
+	Db_User_Name string
+	Db_Password string
 	// DBUrl     string
 	// JWTSecret string
 	// Env       string
@@ -20,14 +22,16 @@ var ENV *config
 
 func loadEnv() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("⚠️  No .env file found. Using system environment variables.")
+		log.Println("No .env file found. Using system environment variables.")
+		os.Exit(1)
 	}
 }
 
 func getEnv(key string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists || value == "" {
-		log.Fatalf("❌ Required environment variable %s is not set or empty", key)
+		log.Fatalf(" Required environment variable %s is not set or empty", key)
+		os.Exit(1)
 	}
 	return value
 }
@@ -38,5 +42,7 @@ func Init() {
 
 	ENV = &config{
 		Port: getEnv("PORT"),
+		Db_User_Name: getEnv("DB_USER_NAME"),
+		Db_Password: getEnv("DB_PASSWORD"),
 	}
 }
